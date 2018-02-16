4a6df4c2dd224cf41fc0866a847e4edf46518af8
jrauber <jrauber@users.noreply.github.com>
2018-02-16 18:28:49 +0100

    remove defaults

--- JasmineAndTypeScript/Properties/AssemblyInfo.cs
+++ JasmineAndTypeScript/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("JasmineAndTypeScript")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("JasmineAndTypeScript")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
